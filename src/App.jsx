266c82570b3683672beea299abecf90eb37ccaaa
Giacomo Curatella <giacur@MacBook-Air-di-Giacomo.local>
2023-12-26 19:35:09 +0100

    primo commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center font-bold text-2xl text-sky-600'>Titolo della pagina</h1>
+    </>
+  )
+}
+
+export default App
