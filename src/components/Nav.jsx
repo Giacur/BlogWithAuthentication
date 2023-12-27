707af9c3745f4a07d5e1eefe9425ec6a8370d8c2
Giacomo Curatella <giacur@MacBook-Air-di-Giacomo.local>
2023-12-27 21:10:48 +0100

    corretta rotta sul click logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ export default function Nav() {
     <nav className="bg-purple-950">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <NavLink
-          to="/"
+          to="articles"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
